9b2e61970eb70e38ac28e40ef95aa6c6d2bb4e4b
Hammed Anuoluwapo Pelumi <hammedanuoluwapopelumi@gmail.com>
2026-01-18 02:25:55 +0000

    feat(cmd): implement list command to display todo items
    - Load items from store and display in formatted list
    - Show completion status with checkmarks (x) or empty boxes
    - Format output as "ID [status] text"

--- cmd/list.go
+++ cmd/list.go
@@ -1,6 +1,5 @@
 /*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2026 tgenericx <hammedanuoluwapopelumi@gmail.com>
 */
 package cmd
 
@@ -10,31 +9,28 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "List all todo items",
+	Run:   runList,
+}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
-	},
+func runList(cmd *cobra.Command, args []string) {
+	items, err := store.Load()
+	if err != nil {
+		cobra.CheckErr(err)
+	}
+
+	for _, item := range items {
+		status := " "
+		if item.Done {
+			status = "x"
+		}
+
+		fmt.Printf("%d [%s] %s\n", item.ID, status, item.Text)
+	}
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
